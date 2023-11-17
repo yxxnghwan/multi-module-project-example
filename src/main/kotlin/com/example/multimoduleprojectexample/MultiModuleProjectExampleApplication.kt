a9ba8cb70c28932cfacad2c916f81687ec763695
yxxnghwan <younghwan960@gmail.com>
2023-11-17 15:31:28 +0900

    basic project init

---
+++ src/main/kotlin/com/example/multimoduleprojectexample/MultiModuleProjectExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.multimoduleprojectexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiModuleProjectExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<MultiModuleProjectExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/multimoduleprojectexample/MultiModuleProjectExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.multimoduleprojectexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiModuleProjectExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
